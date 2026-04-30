a2b2365806e65e118232035f4576425ec7efff41
Guilherme Moreli <es.guilhermemoreli@proton.me>
2026-04-30 20:18:39 -0300

    paginas criadas para cada app

---
+++ pedidos/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from cardapio.views import montar_contexto
+
+
+def index(request):
+    return render(request, 'index.html', montar_contexto('pedidos'))
--- setup/urls.py
+++ setup/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from home import views as home_views
 
 urlpatterns = [
+    path('', home_views.index, name='index'),
+    path('home/', include('home.urls')),
+    path('cardapio/', include('cardapio.urls')),
+    path('produtos/', include('produtos.urls')),
+    path('pedidos/', include('pedidos.urls')),
     path('admin/', admin.site.urls),
 ]
